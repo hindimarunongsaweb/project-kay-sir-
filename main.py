bd5b46fb89e42e501fda7e480861d3598f5caaa8
Elias <johnpaulcauntoy@gmail.com>
2025-04-25 22:04:46 +0800

    added trial.text

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Ask the user for their name
 print("may nag bago ba?")
 name = input("What's your name? ")
-
+num = int(input("Anong Number Paborito mo? "))
 # Print a greeting message
 print("Hello, " + name + "! Nice to meet you.")
+print("yung number mo: ", num)
